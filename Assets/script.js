269763c1d37a25199afb056242ec6b9ddf6417bd
Zo Beki <uobeki.40@gmail.com>
2021-01-12 03:56:01 -0500

    timer added

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,27 @@
-//User presses button to start the game
+//User presses button to start the quiz
+var startQuiz = document.querySelector("#start-button")
 //Once the game has started a timer begins counting and the first random question appears
+var time = document.getElementById('timer')    
 //As the user answers the questions the page changes to the next question
 //If the answer was correct, score increases
 //If the answer was incorrect, time decreases
-//The game is over when the time has run out or all questions answered
+//The quiz is over when the time has run out or all questions answered
+
+//Score
+
+//Time
+startQuiz.addEventListener("click", function countdown(){
+    var timeCount = 90;
+
+    var timeInterval = setInterval(function () {
+        time.innerHTML = timeCount;
+        timeCount--;
+        console.log(timeCount);
+
+        if (timeCount == 0){
+            clearInterval(timeInterval);
+            time.innerHTML="";
+        }
+    },1000);
+
+})
