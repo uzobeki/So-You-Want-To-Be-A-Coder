73a319130cbbbd1cd464922c6d53ede0f8f95116
Zo Beki <uobeki.40@gmail.com>
2021-01-11 22:25:15 -0500

    pseudocode added

---
+++ Assets/script.js
@@ -0,0 +1,6 @@
+//User presses button to start the game
+//Once the game has started a timer begins counting and the first random question appears
+//As the user answers the questions the page changes to the next question
+//If the answer was correct, score increases
+//If the answer was incorrect, time decreases
+//The game is over when the time has run out or all questions answered
